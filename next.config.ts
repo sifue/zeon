4cb13bba59d1bd23fc94aa97ea85cf621164cf4f
Soichiro Yoshimura <yoshimura@soichiro.org>
2025-04-13 01:34:16 +0900

    Google One Tapに対応するためにヘッダを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
