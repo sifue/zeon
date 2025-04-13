71b9254e29a37bbc13539faf6ef1ac09a713c75a
Soichiro Yoshimura <yoshimura@soichiro.org>
2025-04-14 03:18:45 +0900

    buildできなかった問題に対処

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from '@/components/hero';
 import BanMessageToast from '@/components/ban-message-toast';
+import { Suspense } from 'react';
 
 export default async function Home() {
   return (
     <>
-      <BanMessageToast />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BanMessageToast />
+      </Suspense>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         {/* メインコンテンツはここに追加されます */}
